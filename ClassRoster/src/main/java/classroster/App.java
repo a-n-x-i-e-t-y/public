c004d1e0b5c06f8e7551c7dbbb8f8a83ebd88d8b
Alexandru Muresanu <alexandru.gabriel.muresanu@gmail.com>
2022-04-05 16:02:55 -0400

    capitals, now in the Git!

---
+++ ClassRoster/src/main/java/classroster/App.java
@@ -0,0 +1,15 @@
+package classroster;
+
+import Controller.ClassRosterController;
+
+/**
+ *
+ * @author alexg
+ */
+public class App {
+
+    public static void main(String[] args) {
+        ClassRosterController controller = new ClassRosterController();
+        controller.run();
+    }   
+}
