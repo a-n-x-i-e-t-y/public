1bc07d0c2c68f407ee2b5acc3e9f57ef3d8fed96
Alexandru Muresanu <alexandru.gabriel.muresanu@gmail.com>
2022-04-06 15:04:34 -0400

    last mehods

--- DVD/src/main/java/dvd/dao/DVDDaoFileImpl.java
+++ DVD/src/main/java/dvd/dao/DVDDaoFileImpl.java
@@ -5,8 +5,14 @@
 package dvd.dao;
 
 import dvd.dto.DVDObj;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -16,14 +22,12 @@ import java.util.Set;
 public class DVDDaoFileImpl implements DVDDao {
 
     private Map<String, DVDObj> dvdCollection = new HashMap<>();
-    
+
     @Override
     public DVDObj addDVD(String title, DVDObj dvd) {
         DVDObj tempdvd = dvdCollection.put(title, dvd);
         return tempdvd;
     }
-    
-
 
     @Override
     public DVDObj removeDVD(String title) {
@@ -31,58 +35,89 @@ public class DVDDaoFileImpl implements DVDDao {
         dvdCollection.remove(title);
         return tempdvd;
     }
-    
-    
 
     @Override
     public DVDObj editDVD(String title, DVDObj dvd) {
-        
+
         dvdCollection.put(title, dvd);
-        
+
         DVDObj tempdvd = dvdCollection.get(title);
-        
+
         return tempdvd;
-        
-        
+
     }
-    
-    
 
     @Override
     public Set<String> getAllDVDNames() {
-        
+
         Set<String> dvdtitles = dvdCollection.keySet();
         return dvdtitles;
-        
+
     }
 
-    
-    
-    
     @Override
     public DVDObj getDVDAtt(String title) {
-        
-       DVDObj tempdvd = dvdCollection.get(title);
-    
-       return tempdvd;    
+
+        DVDObj tempdvd = dvdCollection.get(title);
+
+        return tempdvd;
     }
 
     @Override
     public DVDObj searchDVD(String title) {
-       DVDObj tempdvd = dvdCollection.get(title);
+        DVDObj tempdvd = dvdCollection.get(title);
+
+        return tempdvd;
+
+    }
+
+    @Override
+    public DVDObj loadLibrary() {
+        try {
+            
+
+        Scanner sc = new Scanner(new BufferedReader(new FileReader("collection.txt")));//to read from file
+        }
+       catch(Exception e){
+           
+       }
+    }
+
+
+
+@Override
+public DVDObj saveLibrary() {
     
-       return tempdvd;   
+        try {//to create file
+            File myObj = new File("collection.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         
+        
+        
+        
+        
+        try {//to write to file
+      FileWriter myWriter = new FileWriter("collection.txt");
+      myWriter.write("Files in Java might be tricky, but it is fun enough!");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-
-    @Override
-    public DVDObj loadDVD() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public DVDObj saveDVD() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    
+    
+    
+    
     }
     
 }
